e40b87840eb4df5e6bf655191f87b03bbd7af576
Marigold <1376579889@qq.com>
2025-05-12 14:02:39 +0800

    pydgc

---
+++ examples/pipelines/ns4gc/run.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+import os
+import sys
+root = os.path.dirname(os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__)))))
+sys.path.append(root)
+os.chdir(os.path.dirname(os.path.abspath(__file__)))
+
+from pydgc.pipelines import NS4GCPipeline
+from pydgc.utils import parse_arguments
+
+# datasets = ["WIKI", "CORA", "ACM", "CITE", "DBLP", "BLOG", "FLICKR", "USPS_3", "HHAR_3", "PUBMED", "ROMAN", "ARXIV"]
+datasets = ["CORA"]
+for dataset in datasets:
+    args = parse_arguments(dataset)
+    pipeline = NS4GCPipeline(args)
+    pipeline.run()
