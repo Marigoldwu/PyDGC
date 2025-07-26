576aa71507419d45fb8a2b3350e9cb5a50397cb9
Marigoldwu <bywu109@163.com>
2025-07-26 20:59:41 +0800

    doc

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -44,7 +44,6 @@ exclude_patterns = []
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
 html_theme = 'sphinx_rtd_theme'
-html_static_path = ['_static']
 html_theme_options = {
     'navigation_depth': 2,  # 导航栏深度
     'collapse_navigation': False,  # 不折叠导航
